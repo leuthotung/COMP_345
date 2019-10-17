6dd4c9a44a0fa83310e052039a32f2a8b4a2de66
pigooo1 <46521750+pigooo1@users.noreply.github.com>
2019-10-17 14:25:50 -0400

    12345

--- Cards.h
+++ Cards.h
@@ -4,6 +4,8 @@
 enum cardType {infantry,artillery,cavalry}; //enum 3 type of cards
 
 class Card {
+private:
+	cardType* type;
 public:
 	Card(cardType t);
 	void display();
@@ -12,8 +14,7 @@ public:
 	~Card();
 	//why two destructor: when the deckVector is created, I will use 'temp' as variable name to  new a Card object to push,
 	//however when the 'temp' point to another object, the former one will call destructructor automatically;
-private:
-	cardType* type;
+
 };
 
 class Deck {
@@ -30,6 +31,7 @@ public:
 };
 
 class Hand {
+private:
 	int* numOfInf;
 	int* numOfCav;
 	int* numOfArt;
