29bf9fc0ed49afe5d7a374960dbc55660b072d77
Tung <leudeptrai@gmail.com>
2019-11-14 01:32:23 -0500

    added adapter

--- MapLoaderDriver.cpp
+++ MapLoaderDriver.cpp
@@ -6,12 +6,21 @@
 
 using namespace std;
 
-/*
 int main() {
-    Map a = readMapFile("../Maps/europe.map");
-    a.print();
+/*    Map a = readMapFile("../Maps/europe.map");
+    a.print()*/;
     //read a fail map
-    Map b = readMapFile("../Maps/test.map");
+    /*Map b = readMapFile("../Maps/test.map");
+    return 0;*/
+
+ /*   Map a = readConquestMapFile("../ConquestMap/Alabama.map");
+    a.print();*/
+ //MapLoader mapload;
+// Map a = mapload.readMapFile("../Maps/europe.map");
+ //a.print();
+ Adaptee conquest;
+ Adapter readConquest(conquest);
+ Map b = readConquest.readMapFile("../ConquestMap/Alabama.map");
+ b.print();
     return 0;
 }
-*/
